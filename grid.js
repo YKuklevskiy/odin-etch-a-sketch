a83c3b11495236203517a5dc5e736ec7d27eeae5
Ykuklevskiy <73666754+YKuklevskiy@users.noreply.github.com>
2026-05-12 20:34:55 +0200

    Implement grid generation via JS

--- grid.js
+++ grid.js
@@ -1,1 +1,25 @@
-console.log("hello")
+const gridContainer = document.getElementById("grid-container");
+const GRID_SIZE = 16;
+const GRID_ROW_CLASS = "grid-row";
+const CELL_CLASS = "grid-cell";
+
+const gridRowId = (i) => `grid-row-${i}`;
+const cellId = (i, j) => `cell-${i}-${j}`;
+
+let grid = [];
+for (let i = 0; i < GRID_SIZE; i++) {
+    let gridRow = document.createElement("div");
+    gridRow.id = gridRowId(i);
+    gridRow.classList.add(GRID_ROW_CLASS);
+
+    grid.push([]);
+    for (let j = 0; j < GRID_SIZE; j++) {
+        let cell = document.createElement("div");
+        cell.id = cellId(i, j);
+        cell.classList.add(CELL_CLASS);
+
+        grid.at(-1).push(cell);
+        gridRow.appendChild(cell);
+    }
+    gridContainer.appendChild(gridRow);
+}
