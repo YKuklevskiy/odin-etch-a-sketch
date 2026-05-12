eefcf913ff684b5a5bd7d45ed3af03a9036a608f
Ykuklevskiy <73666754+YKuklevskiy@users.noreply.github.com>
2026-05-12 21:32:48 +0200

    Extract grid generation into a function

--- grid.js
+++ grid.js
@@ -8,23 +8,28 @@ const CELL_FILLED_CLASS = "grid-cell-filled";
 const gridRowId = (i) => `grid-row-${i}`;
 const cellId = (i, j) => `cell-${i}-${j}`;
 
-let grid = [];
-for (let i = 0; i < GRID_SIZE; i++) {
-    let gridRow = document.createElement("div");
-    gridRow.id = gridRowId(i);
-    gridRow.classList.add(GRID_ROW_CLASS);
-
-    grid.push([]);
-    for (let j = 0; j < GRID_SIZE; j++) {
-        let cell = document.createElement("div");
-        cell.id = cellId(i, j);
-        cell.classList.add(CELL_CLASS);
-
-        grid.at(-1).push(cell);
-        gridRow.appendChild(cell);
+function generateGrid(size) {
+    let grid = [];
+    for (let i = 0; i < size; i++) {
+        let gridRow = document.createElement("div");
+        gridRow.id = gridRowId(i);
+        gridRow.classList.add(GRID_ROW_CLASS);
+        
+        grid.push([]);
+        for (let j = 0; j < size; j++) {
+            let cell = document.createElement("div");
+            cell.id = cellId(i, j);
+            cell.classList.add(CELL_CLASS);
+            
+            grid.at(-1).push(cell);
+            gridRow.appendChild(cell);
+        }
+        gridContainer.appendChild(gridRow);
     }
-    gridContainer.appendChild(gridRow);
+    return grid;
 }
+
+let grid = generateGrid(GRID_SIZE);
 
 function onCellHover(event) {
     if(!event.target.classList.contains(CELL_CLASS))
